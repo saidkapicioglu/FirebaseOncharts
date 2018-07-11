9ff0620b5d75bb9ee1c9835bf4d7d80488d9bcf5
saidkapicioglu <saidkapicioglu@gmail.com>
2018-07-12 02:49:02 +0300

    Firebase

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCHmvg4ks9dMZoqIp6UqK3zzOhHxq5byHA",
+    authDomain: "fuzzylogicproject-49e07.firebaseapp.com",
+    databaseURL: "https://fuzzylogicproject-49e07.firebaseio.com",
+    projectId: "fuzzylogicproject-49e07",
+    storageBucket: "fuzzylogicproject-49e07.appspot.com",
+    messagingSenderId: "335011642713"
+  };
+  firebase.initializeApp(config);
